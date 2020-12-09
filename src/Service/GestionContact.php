f35dec68c37dcd56db2c0a57264c7a0debbea78b
Thibault Laude <laudethibault_pro@outlook.fr>
2020-12-09 17:25:33 +0100

    Ajout du service

---
+++ src/Service/GestionContact.php
@@ -0,0 +1,46 @@
+<?php
+
+use App\Entity\Contact;
+use Twig\Environment;
+use Doctrine\Persistence\ManagerRegistry ;
+use App\Repository\ContactRepository;
+/**
+ * Description of GestionContact
+ *
+ * @author Benoît
+ */
+class GestionContact {
+//documentation : https://swiftmailer.symfony.com/docs/sending.html
+    private \Swift_Mailer $mail;
+    private Environment $environnementTwig;
+    private ManagerRegistry $doctrine;
+    private ContactRepository $repo;
+
+    function __construct(\Swift_Mailer $mail, Environment $environnementTwig, ManagerRegistry $doctrine, ContactRepository $repo) {
+        $this->mail = $mail;
+        $this->environnementTwig = $environnementTwig;
+        $this->doctrine=$doctrine;
+        $this->repo=$repo;
+    }
+
+    public function envoiMailContact(Contact $message) {
+        //$titre = ($contact->getTitre() == 'M') ? ('Monsieur') : ('Madame');
+        $email = (new \Swift_Message('Demande de renseignement'))
+                -setFrom([$message->getMail()=>'Nouvelle demande'])
+                //->setFrom(['contact@benoitroche.fr'=> 'Benoît Roche Symfony'])
+                ->setTo(['laudethibault83@gmail.com'=> 'Thibault Laude Symfony']);
+           //$img=  $email->embed(\Swift_Image::fromPath('build/images/symfony.png'));
+           $email->setBody(
+                        $this->environnementTwig->render(
+                                // templates/emails/registration.html.twig
+                                'mail/mail.html.twig',
+                                ['message' => $message]
+                        ),
+                        'text/html'
+                );
+            $email->attach(\Swift_Attachment::fromPath('documents/presentation.pdf'));
+        $this->mail->send($email);
+    }  
+}
+
+
