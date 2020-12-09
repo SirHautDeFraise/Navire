bbef07bf5255fbe094ffd5a20f8d751c9e6a03f3
Thibault Laude <laudethibault_pro@outlook.fr>
2020-12-09 16:10:31 +0100

    Rajout des uses

--- src/Form/MessageType.php
+++ src/Form/MessageType.php
@@ -6,6 +6,9 @@ use App\Entity\Message;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 
 class MessageType extends AbstractType
 {
