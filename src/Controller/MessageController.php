460ea6e6fe51ed889a9582ba5a4141086035b74f
Thibault Laude <laudethibault_pro@outlook.fr>
2020-12-10 11:03:35 +0100

    Debut Partie 2 Doctrine

--- src/Controller/MessageController.php
+++ src/Controller/MessageController.php
@@ -1,45 +1,45 @@
 <?php
-
+//
 namespace App\Controller;
-
+//
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\Request;
-use App\Service\GestionContact;
-
-/**
- * @Route("/message/", name="message_")
- */
+//use Symfony\Component\HttpFoundation\Response;
+//use Symfony\Component\Routing\Annotation\Route;
+//use Symfony\Component\HttpFoundation\Request;
+//use App\Service\GestionContact;
+//
+///**
+// * @Route("/message/", name="message_")
+// */
 class MessageController extends AbstractController {
-
-  public function index(): Response {
-    return $this->render('message/index.html.twig', [
-                'controller_name' => 'MessageController',
-    ]);
-  }
-
-  /**
-   * @Route("contact", name="messagecontact")
-   */
-  public function Formulaire(Request $request, GestionContact $gestionContact): Response {
-    $message = new Message();
-
-    $form = $this->createForm(MessageType::class, $message);
-    $form->handleRequest($request);
-
-    if ($form->isSubmitted() && $form->isValid()) {
-      $message = $form->getData();
-
-      $gestionContact->envoiMailContact($message);
-
-
-      return $this->redirectToRoute("home");
-    }
-
-    return $this->render('message/contact.hmtl.twig', [
-                'form' => $form->createView(),
-    ]);
-  }
-
+//
+//  public function index(): Response {
+//    return $this->render('message/index.html.twig', [
+//                'controller_name' => 'MessageController',
+//    ]);
+//  }
+//
+//  /**
+//   * @Route("contact", name="messagecontact")
+//   */
+//  public function Formulaire(Request $request, GestionContact $gestionContact): Response {
+//    $message = new Message();
+//
+//    $form = $this->createForm(MessageType::class, $message);
+//    $form->handleRequest($request);
+//
+//    if ($form->isSubmitted() && $form->isValid()) {
+//      $message = $form->getData();
+//
+//      $gestionContact->envoiMailContact($message);
+//
+//
+//      return $this->redirectToRoute("home");
+//    }
+//
+//    return $this->render('message/contact.hmtl.twig', [
+//                'form' => $form->createView(),
+//    ]);
+//  }
+//
 }
--- src/Service/GestionContact.php
+++ src/Service/GestionContact.php
@@ -1,49 +1,50 @@
 <?php
-
-
-namespace App\Service;
-
-use App\Entity\Contact;
-use Twig\Environment;
-use Doctrine\Persistence\ManagerRegistry ;
-use App\Repository\MessageRepository;
-/**
- * Description of GestionContact
- *
- * @author Benoît
- */
-class GestionContact {
-//documentation : https://swiftmailer.symfony.com/docs/sending.html
-    private \Swift_Mailer $mail;
-    private Environment $environnementTwig;
-    private ManagerRegistry $doctrine;
-    private MessageRepository $repo;
-
-    function __construct(\Swift_Mailer $mail, Environment $environnementTwig, ManagerRegistry $doctrine, MessageRepository $repo) {
-        $this->mail = $mail;
-        $this->environnementTwig = $environnementTwig;
-        $this->doctrine=$doctrine;
-        $this->repo=$repo;
-    }
-
-    public function envoiMailContact(Contact $message) {
-        //$titre = ($contact->getTitre() == 'M') ? ('Monsieur') : ('Madame');
-        $email = (new \Swift_Message('Demande de renseignement'))
-                -setFrom([$message->getMail()=>'Nouvelle demande'])
-                //->setFrom(['contact@benoitroche.fr'=> 'Benoît Roche Symfony'])
-                ->setTo(['laudethibault83@gmail.com'=> 'Thibault Laude Symfony']);
-           //$img=  $email->embed(\Swift_Image::fromPath('build/images/symfony.png'));
-           $email->setBody(
-                        $this->environnementTwig->render(
-                                // templates/emails/registration.html.twig
-                                'mail/mail.html.twig',
-                                ['message' => $message]
-                        ),
-                        'text/html'
-                );
-            $email->attach(\Swift_Attachment::fromPath('documents/presentation.pdf'));
-        $this->mail->send($email);
-    }  
-}
+//
+//
+//namespace App\Service;
+//
+//use App\Entity\Contact;
+//use Twig\Environment;
+//use Doctrine\Persistence\ManagerRegistry ;
+//use App\Repository\MessageRepository;
+//use Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle;
+///**
+// * Description of GestionContact
+// *
+// * @author Benoît
+// */
+//class GestionContact {
+////documentation : https://swiftmailer.symfony.com/docs/sending.html
+//    private \Swift_Mailer $mail;
+//    private Environment $environnementTwig;
+//    private ManagerRegistry $doctrine;
+//    private MessageRepository $repo;
+//
+//    function __construct(\Swift_Mailer $mail, Environment $environnementTwig, ManagerRegistry $doctrine, MessageRepository $repo) {
+//        $this->mail = $mail;
+//        $this->environnementTwig = $environnementTwig;
+//        $this->doctrine=$doctrine;
+//        $this->repo=$repo;
+//    }
+//
+//    public function envoiMailContact(Contact $message) {
+//        //$titre = ($contact->getTitre() == 'M') ? ('Monsieur') : ('Madame');
+//        $email = (new \Swift_Message('Demande de renseignement'))
+//                -setFrom([$message->getMail()=>'Nouvelle demande'])
+//                //->setFrom(['contact@benoitroche.fr'=> 'Benoît Roche Symfony'])
+//                ->setTo(['laudethibault83@gmail.com'=> 'Thibault Laude Symfony']);
+//           //$img=  $email->embed(\Swift_Image::fromPath('build/images/symfony.png'));
+//           $email->setBody(
+//                        $this->environnementTwig->render(
+//                                // templates/emails/registration.html.twig
+//                                'mail/mail.html.twig',
+//                                ['message' => $message]
+//                        ),
+//                        'text/html'
+//                );
+////            $email->attach(Swift_Attachment::fromPath('documents/presentation.pdf'));
+//        $this->mail->send($email);
+//    }  
+//}
 
 
